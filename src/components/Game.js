ca48484b05aff2232df589369567429e24388607
Annabelle Caspersz <annabelle@futurefriendly.team>
2021-10-07 18:21:32 +1100

    updating JS functions to react in Game section

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,11 +12,11 @@ const Game = () => {
 
   const [play] = useSound(uh, {volume: 0.3});
 
-  function randomTime(min, max) {
+  const randomTime = (min, max) => {
     return Math.round(Math.random() * (max - min) + min);
   }
 
-  function randomHole(holes) {
+  const randomHole = (holes) => {
     const idx = Math.floor(Math.random() * holes.length);
     const hole = holes[idx];
     if (hole === lastHole) {
@@ -26,7 +26,7 @@ const Game = () => {
     return hole;
   }
 
-  function peep() {
+  const peep = () => {
     const time = randomTime(300, 1000);
     const hole = randomHole(holes);
     hole.classList.add('up');
@@ -36,46 +36,49 @@ const Game = () => {
     }, time);
   }
 
-  function startGame() {
-    scoreBoard.innerHTML = 0;
+  const startGame = () => {
+    scoreBoard = 0;
     timeUp = false;
     score = 0;
     peep();
     setTimeout(() => timeUp = true, 10000)
   }
 
-  function bonk(e) {
+  const bonk = (e) => {
     // if(!e.isTrusted) return;
     score++;
     this.parentNode.classList.remove('up');
     scoreBoard.innerHTML = score;
   }
 
+  
+  console.log("score", score);
+
   elfs.forEach(elf => elf.addEventListener('click', bonk));
   return (
-    <div class="game-container">
-      <div class="score-start">
+    <div className="game-container">
+      <div className="score-start">
         <h3>Score: <span class="score">0</span></h3>
     <button onClick={startGame}>Start!</button>
       </div>
-      <div class="game">
-        <div class="hole hole1">
-          <div class="elf" onClick={play}></div>
+      <div className="game">
+        <div className="hole hole1">
+          <div className="elf" onClick={play}></div>
         </div>
-        <div class="hole hole2">
-          <div class="elf" onClick={play}></div>
+        <div className="hole hole2">
+          <div className="elf" onClick={play}></div>
         </div>
-    <div class="hole hole3">
-      <div class="elf" onClick={play}></div>
+    <div className="hole hole3">
+      <div className="elf" onClick={play}></div>
     </div>
-    <div class="hole hole4">
-      <div class="elf" onClick={play}></div>
+    <div className="hole hole4">
+      <div className="elf" onClick={play}></div>
     </div>
-    <div class="hole hole5">
-      <div class="elf" onClick={play}></div>
+    <div className="hole hole5">
+      <div className="elf" onClick={play}></div>
     </div>
-    <div class="hole hole6">
-      <div class="elf" onClick={play}></div>
+    <div className="hole hole6">
+      <div className="elf" onClick={play}></div>
     </div>
   </div>
     </div>
