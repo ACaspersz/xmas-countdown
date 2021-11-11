5a364060d6d79519886afe005b9ab5b4612f78ea
Annabelle Caspersz <annabelle@futurefriendly.team>
2021-11-11 19:24:29 +1100

    corrected conditional on clock description

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,6 +1,9 @@
 import React, {Fragment} from 'react';
 
 const Clock = ({ timerDays, timerHours, timerMinutes, timerSeconds }) => {
+  const isItFinished = () => {
+    timerDays === 0 && timerHours === 0 && timerMinutes === 0 timerSeconds === 0 ? true : false;
+  }
   return <Fragment>
     <section className="timer-container">
       <section className="timer">
@@ -26,7 +29,7 @@ const Clock = ({ timerDays, timerHours, timerMinutes, timerSeconds }) => {
           </section>
         </div>
       </section>
-      { timerDays === 0 && timerHours === 0 ? <h3 ><span className="subtitle animated bounce">hope your christmas dreams come true ❤️</span></h3> : <h3><span className="subtitle animated bounce">until FF Xmas adventure</span></h3>}
+      { timerDays === 0 && timerHours === 0 && timerMinutes === 0 && timerSeconds === 0 ? <h3 ><span className="subtitle animated bounce">hope your christmas dreams come true ❤️</span></h3> : <h3><span className="subtitle animated bounce">until FF Xmas adventure</span></h3>}
     </section>
   </Fragment>
 }
