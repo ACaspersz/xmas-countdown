359d2fb17344cf340ba9cdb27666a7f3e4f3405b
Annabelle Caspersz <annabelle@futurefriendly.team>
2021-11-12 11:34:14 +1100

    corrected gif and duplicated

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -27,6 +27,12 @@ const Clock = ({ timerDays, timerHours, timerMinutes, timerSeconds }) => {
         </div>
       </section>
       { timerDays === 0 && timerHours === 0 && timerMinutes === 0 && timerSeconds === 0 ? <h3 ><span className="subtitle animated bounce">hope your christmas dreams come true ❤️</span></h3> : <h3><span className="subtitle animated bounce">until FF Xmas adventure</span></h3>}
+      <div className="kermit-wrapper">
+      { timerDays === 0 && timerHours === 0 && timerMinutes === 0 && timerSeconds === 0 ? <div className="kermit"><div><iframe title="kermit-gif" src="https://giphy.com/embed/etOX3h7ApZuDe7Fc5w" width="200px" height="200px" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div></div>
+      : ''}
+      { timerDays === 0 && timerHours === 0 && timerMinutes === 0 && timerSeconds === 0 ? <div className="kermit-right"><div><iframe title="kermit-gif" src="https://giphy.com/embed/etOX3h7ApZuDe7Fc5w" width="200px" height="200px" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div></div>
+      : ''}
+      </div>
     </section>
   </Fragment>
 }
